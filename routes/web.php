dd3914f3b7a988fb1d66a54a37d136bb0016c4c3
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-08-04 14:51:15 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/teste', function () {
+    $products = Product::all();
+    echo "<pre>";
+    print_r($products->toArray());
+});
