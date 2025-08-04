667b5dd70ab4b05be3467d2a2f7a939bbf3835b9
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-08-04 15:03:45 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Models\Product;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/teste', function () {
-    $products = Product::all();
-    echo "<pre>";
-    print_r($products->toArray());
-});
+Route::get('/', [MainController::class, 'index']);
