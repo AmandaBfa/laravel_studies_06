7803a722e92c7f2d766c1e14124b5bcab0fffe6c
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-08-05 09:19:16 -0300

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,17 +2,62 @@
 
 namespace App\Http\Controllers;
 
-// use App\Models\Product;
-
-use App\Models\TesteModel;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        $results = TesteModel::all()->toArray();
+        $results = Product::all()->toArray();
+
+        // // buscar todos os dados dos produtos
+        // $results = Product::all(); // SELLECT * FROM products;
+        // echo "<pre>";
+        // print_r($results);
+
+
+        // echo $results[0]->product_name;
+
+        // foreach ($results as $product) {
+        //     echo $product->product_name . "<br>";
+        // }
+
+
+        // // buscar todos os dados com um array associativo
+        // $results = Product::all()->toArray(); // SELLECT * FROM products;
+        // echo "<pre>";
+        // print_r($results);
+
+        // // retornar os resultados como um array de objetos stdClass
+        // $results = $this->ArrayOfObject(Product::all()->toArray());
+        // $this->showData($results);
+
+        // // buscar produtos ordernados por nome alfabéticamente
+        // $results = Product::orderBy('product_name')->get()->toArray();
+        // $this->showData($results);
+
+        // // buscar os tres primeiros produtos
+        // $results = Product::limit(3)->get()->toArray();
+        // $this->showData($results);
+
+        // // buscar um produto pelo seu ID
+        $results = Product::find(10)->toArray();
+        $this->showData($results);
+    }
+
+    private function showData($data)
+    {
         echo "<pre>";
-        print_r($results);
+        print_r($data);
+    }
+
+    private function ArrayOfObject($data)
+    {
+        $tmp = [];
+        foreach ($data as $key => $value) {
+            $tmp[] = (object) $value; // converte o array associativo em um objeto
+        }
+        return $tmp;
     }
 }
