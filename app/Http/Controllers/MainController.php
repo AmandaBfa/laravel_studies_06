73cd9c20a0329f02160d816d127f5c2ef35419d8
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-08-06 09:05:54 -0300

    011

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -43,6 +43,38 @@ class MainController extends Controller
         // }
     }
 
+    public function OneToMany()
+    {
+        // // buscar o id e o nome do cliente e todos os seus telefones
+        // $client1 = Client::find(10); // supondo que o cliente com ID 12 exista
+        // $phones = $client1->phones; // pega todos os telefones do cliente
+        // echo "Cliente: " . $client1->client_name . "<br>";
+        // echo "Telefone(s): <br>";
+        // foreach ($phones as $phone) {
+        //     echo " - " . $phone->phone_number . "<br>";
+        // }
+
+        // // outra forma é usando o método with()
+        // $client2 = Client::with('phones')->find(12);
+        // echo "<br>";
+        // echo "Cliente: " . $client2->client_name . "<br>";
+        // echo "Telefone: <br>";
+        // foreach ($client2->phones as $phone) {
+        //     echo $phone->phone_number . "<br>";
+        // }
+
+        // buscar todos os clientes e os seus telefones
+        // $clients = Client::with('phones')->get();
+        // foreach ($clients as $client) {
+        //     echo "<br>";
+        //     echo "Cliente: " . $client->client_name . "<br>";
+        //     echo "Telefone: <br>";
+        //     foreach ($client->phones as $phone) {
+        //         echo $phone->phone_number . "<br>";
+        //     }
+        // }
+    }
+
 
 
     private function showData($data)
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'index']);
 Route::get('/one_to_one', [MainController::class, 'OneToOne']);
+Route::get('/one_to_many', [MainController::class, 'OneToMany']);
